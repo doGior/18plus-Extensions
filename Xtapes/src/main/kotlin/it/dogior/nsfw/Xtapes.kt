1be830e89d1c7265bce9025906916d5cf7615fb4
doGior <43815981+doGior@users.noreply.github.com>
2026-04-08 19:36:31 +0200

    Updated url

--- Xtapes/src/main/kotlin/it/dogior/nsfw/Xtapes.kt
+++ Xtapes/src/main/kotlin/it/dogior/nsfw/Xtapes.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 
 class Xtapes : MainAPI() {
-    override var mainUrl = "https://xtapes.in"
+    override var mainUrl = "https://xtapes.la"
     override var name = "Xtapes"
     override val hasMainPage = true
     override var lang = "en"
